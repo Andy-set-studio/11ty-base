fcf58d7d27b9ee57fec470f1ee94d3a07ab7c8af
Andy Bell <me@andy-bell.co.uk>
2021-04-26 10:34:44 +0100

    Clean up Sass task

--- gulp-tasks/sass.js
+++ gulp-tasks/sass.js
@@ -9,7 +9,7 @@ sassProcessor.compiler = require('sass');
 const isProduction = process.env.NODE_ENV === 'production';
 
 // An array of outputs that should be sent over to includes
-const criticalStyles = ['critical.scss', 'home.scss', 'page.scss', 'work-item.scss'];
+const criticalStyles = ['critical.scss'];
 
 // Takes the arguments passed by `dest` and determines where the output file goes
 const calculateOutput = ({history}) => {
